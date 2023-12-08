4fe01c51906c9f1758d3a932a6868e8bf7b035e9
e-s-a-h-C <chaseneumann01@gmail.com>
2023-12-08 17:02:09 -0600

    moving function around again

--- js/ride.js
+++ js/ride.js
@@ -1,6 +1,31 @@
 /*global WildRydes _config*/
+var WildRydes = window.WildRydes || {};
+WildRydes.map = WildRydes.map || {};
+let map;
 
+function renderWeather({ current }) {
+    renderCurrentWeather(current)
+}
 
+function setValue(selector, value, { parent = document } = {}) {
+    parent.querySelector(`[data-${selector}]`).textContent = value
+}
+  
+function getIconUrl(iconCode) {
+    return `icons/${ICON_MAP.get(iconCode)}.svg`
+}
+  
+const currentIcon = document.querySelector("[data-current-icon]")
+function renderCurrentWeather(current) {
+    currentIcon.src = getIconUrl(current.iconCode)
+    setValue("current-temp", current.currentTemp)
+    setValue("current-high", current.highTemp)
+    setValue("current-low", current.lowTemp)
+    setValue("current-fl-high", current.highFeelsLike)
+    setValue("current-fl-low", current.lowFeelsLike)
+    setValue("current-wind", current.windSpeed)
+    setValue("current-precip", current.precip)
+}
 
 (function rideScopeWrapper($) {
     var authToken;
@@ -54,6 +79,16 @@
         console.log(pickupLocation);
         //  get the local weather, find nearby restaurants, movies
         // getWeather(pickupLocation, unicorn)
+        getWeather(
+            coords.latitude,
+            coords.longitude,
+            Intl.DateTimeFormat().resolvedOptions().timeZone
+          )
+            .then(renderWeather)
+            .catch(e => {
+              console.error(e)
+              alert("Error getting weather.")
+            })
 
         animateArrival(function animateCallback() {
             displayUpdate(unicorn.Name + ' has arrived. Giddy up!', unicorn.Color);
