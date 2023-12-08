b9dc3206fc82dd889cf486a5205430af32f689ab
e-s-a-h-C <chaseneumann01@gmail.com>
2023-12-08 00:17:33 -0600

    moving function around again (hopefully this works(probalby not))

--- js/main.js
+++ js/main.js
@@ -1,1 +1,48 @@
 "use strict";!function(){var n=$("html"),t=function(){$(".btn-menu").on("click",function(t){t.preventDefault(),n.toggleClass("menu-opened")})},e=function(){t()};e()}();
+
+import { getWeather } from "./weather";
+
+navigator.geolocation.getCurrentPosition(positionSuccess, positionError)
+
+function positionSuccess({ coords }) {
+    getWeather(
+      coords.latitude,
+      coords.longitude,
+      Intl.DateTimeFormat().resolvedOptions().timeZone
+    )
+      .then(renderWeather)
+      .catch(e => {
+        console.error(e)
+        alert("Error getting weather.")
+      })
+}
+
+function positionError() {
+    alert(
+      "There was an error getting your location. Please allow us to use your location and refresh the page."
+    )
+}
+
+function renderWeather({ current }) {
+    renderCurrentWeather(current)
+}
+
+function setValue(selector, value, { parent = document } = {}) {
+    parent.querySelector(`[data-${selector}]`).textContent = value
+}
+  
+function getIconUrl(iconCode) {
+    return `icons/${ICON_MAP.get(iconCode)}.svg`
+}
+  
+const currentIcon = document.querySelector("[data-current-icon]")
+function renderCurrentWeather(current) {
+    currentIcon.src = getIconUrl(current.iconCode)
+    setValue("current-temp", current.currentTemp)
+    setValue("current-high", current.highTemp)
+    setValue("current-low", current.lowTemp)
+    setValue("current-fl-high", current.highFeelsLike)
+    setValue("current-fl-low", current.lowFeelsLike)
+    setValue("current-wind", current.windSpeed)
+    setValue("current-precip", current.precip)
+}
--- js/weather.js
+++ js/weather.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export function getWeather(lat, lon) {
+export function getWeather(lat, lon, timezone) {
   return axios
     .get(
       "https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime",
@@ -8,6 +8,7 @@ export function getWeather(lat, lon) {
         params: {
           latitude: lat,
           longitude: lon,
+          timezone,
         },
       }
     )
