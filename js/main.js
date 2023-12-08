17771a2aeb7dc5ef66fff29ede21ccb40787fd41
e-s-a-h-C <chaseneumann01@gmail.com>
2023-12-08 16:56:26 -0600

    updating ride and main js files

--- js/main.js
+++ js/main.js
@@ -2,3 +2,53 @@
 
 // import { getWeather } from "./weather";
 
+import { getWeather } from "./js/weather.js"
+
+var WildRydes = window.WildRydes || {};
+WildRydes.map = WildRydes.map || {};
+let map;
+
+navigator.geolocation.getCurrentPosition(positionSuccess, positionError)
+
+function positionSuccess({ coords }) {
+    getWeather(
+      coords.latitude,
+      coords.longitude,
+      Intl.DateTimeFormat().resolvedOptions().timeZone
+    )
+      .then(renderWeather)
+      .catch(e => {
+        console.error(e)
+        alert("Error getting weather.")
+      })
+}
+
+function positionError() {
+    alert(
+      "There was an error getting your location. Please allow us to use your location and refresh the page."
+    )
+}
+
+function renderWeather({ current }) {
+    renderCurrentWeather(current)
+}
+
+function setValue(selector, value, { parent = document } = {}) {
+    parent.querySelector(`[data-${selector}]`).textContent = value
+}
+  
+function getIconUrl(iconCode) {
+    return `icons/${ICON_MAP.get(iconCode)}.svg`
+}
+  
+const currentIcon = document.querySelector("[data-current-icon]")
+function renderCurrentWeather(current) {
+    currentIcon.src = getIconUrl(current.iconCode)
+    setValue("current-temp", current.currentTemp)
+    setValue("current-high", current.highTemp)
+    setValue("current-low", current.lowTemp)
+    setValue("current-fl-high", current.highFeelsLike)
+    setValue("current-fl-low", current.lowFeelsLike)
+    setValue("current-wind", current.windSpeed)
+    setValue("current-precip", current.precip)
+}
