7e109bde393e712e71be9db0cd76fd8328859c88
e-s-a-h-C <chaseneumann01@gmail.com>
2023-12-08 00:59:20 -0600

    moved js files outside of js folder

--- ride.js
+++ ride.js
@@ -6,22 +6,25 @@ let map;
 
 //import axios from "axios"
 const apiurl = 'https://api.open-meteo.com/v1/forecast?hourly=temperature_2m,apparent_temperature,precipitation,weathercode,windspeed_10m&daily=weathercode,temperature_2m_max,temperature_2m_min,apparent_temperature_max,apparent_temperature_min,precipitation_sum&current_weather=true&temperature_unit=fahrenheit&windspeed_unit=mph&precipitation_unit=inch&timeformat=unixtime'
-fetch(apiurl)
-.then(response => {
-    if (!response.ok) {
-        throw new Error('Network response was not ok');
-    }
-    return response.json();
-})
-.then(data => {
-    return {
-        current: parseCurrentWeather(data),
-    }
-})
 function getWeather(lat, lon, timezone) {
+    params: {
         latitude = lat,
         longitude = lon,
         timezone,
+    }
+
+    fetch(apiurl)
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Network response was not ok');
+        }
+        return response.json();
+    })
+    .then(data => {
+        return {
+            current: parseCurrentWeather(data),
+        }
+    })
 }
 
 function parseCurrentWeather({ current_weather, daily }) {
